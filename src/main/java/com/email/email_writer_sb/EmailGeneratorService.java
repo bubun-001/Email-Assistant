c991b2339683377b1039bb15f2890f247e1e500b
Lala Amartya <amartya.chand@gmail.com>
2025-05-11 11:30:52 +0530

    two methods for crafting request

--- src/main/java/com/email/email_writer_sb/EmailGeneratorService.java
+++ src/main/java/com/email/email_writer_sb/EmailGeneratorService.java
@@ -2,6 +2,11 @@ package com.email.email_writer_sb;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 //This is the service class where the main logic is written . the API requests are dispatched by the controller to the service layer
 @Service
 public class EmailGeneratorService {
@@ -10,6 +15,29 @@ public class EmailGeneratorService {
         //Build the prompt
         String prompt = buildPrompt(emailRequest);
         //Craft a request
+        Map<String, Object>requestbody = Map.of(
+             "contents",new Object[]{
+                     Map.of("parts",new Object[]{
+                             Map.of("text",prompt)
+                     })
+                }
+        );
+        //when we use the Map.of() ->it becomes immutable and further nothing can be changed but in hashmap
+        //it can be changed later , thats why hashmap is mostly used.
+//        Map<String,Object>textMap = new HashMap<>();
+//        textMap.put("text" , prompt);
+//
+//        Map<String , Object> partsMap= new HashMap<>();
+//        partsMap.put("parts" , List.of(textMap));
+//
+//        List<Map<String , Object>>contentList = new ArrayList<>();
+//        contentList.add(partsMap);
+//
+//        Map<String , Object> requestBody = new HashMap<>();
+//        requestBody.put("contents" , contentList);
+//        return requestBody;
+
+
         //Do req and get res
         //Return the res
     }
